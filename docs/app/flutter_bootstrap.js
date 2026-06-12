d3cca8bf063355e35491e167a4b754b14c53d84c
Luis Ouriach <louriach@gmail.com>
2026-06-12 20:48:52 +0100

    fix(web): fix autocapitalize param, 8-digit OTP field, text keyboard

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3897727645" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3236040337" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
